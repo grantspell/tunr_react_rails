a4c66616bb5cc5417ea4f1c427aabebe6447bb04
grantspell <spellgrant@gmail.com>
2017-10-31 14:14:15 -0400

    HEROKU DEPLOYED

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,31 +1,44 @@
 class Api::SongsController < ApplicationController
-        def index
-          @songs = Song.all
-          render json: @songs
-        end
-      
-        def create
-          @song = Song.create!(song_params)
-        end
-      
-        def show
-          @song = Song.find(params[:id])
-          render json: @song
-        end
-      
-        def update
-          @song = Song.find(params[:id])
-          @song.update!(song_params)
-        end
-      
-        def destroy
-          @song = Song.find(params[:id])
-          @song.destroy
-        end
-      
-        private
-      
-        def song_params
-          params.require(:song).permit(:title, :album, :preview_url, :artist_id)
-        end
+    
+    # GET '/api/artists/:id/songs'
+    def index
+       @songs = Song.all
+       render json: @songs
+     end
+
+    # GET '/api/artists/:id/songs/:id'
+     def show
+       @song = Song.find(params[:id])
+       render json: @song
+     end
+    
+    # POST '/api/artists/:id/songs'
+     def create
+        @artists = Artist.find(params[:artist_id])
+        @song = Song.new(song_params)
+
+        @artist.songs << @song
+        @artist.save!
+
+        render status: :ok
+     end
+    
+     # PATCH / PUT '/api/artist/:id/songs/:id'
+     def update
+       @song = Song.find(params[:id])
+       @song.update!(song_params)
+
+       render json: @song
+     end
+    
+     def destroy
+       Song.find(params[:id]).delete
+       render status: :ok
+     end
+    
+     private
+    
+     def song_params
+       params.require(:song).permit(:title, :album, :preview_url)
+     end
 end
